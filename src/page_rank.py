bb98a518f0ea5a370cd2124268d200760b5006da
pdhar <pranav.dhar2@gmail.com>
2015-12-05 16:07:18 -0500

    page rank algo - finishing touches

--- src/page_rank.py
+++ src/page_rank.py
@@ -31,53 +31,99 @@ def save_word_dict(text):
       continue
     proc_text.append(proc_sentence)
 
-  print proc_text
-
   dictionary = corpora.Dictionary(proc_text)
   # dictionary.save(os.pardir + '/data/text.dict')
-  return [dictionary, proc_text]
+  return [dictionary, proc_text, sentences]
 
-def start_page_rank():
+def start_page_rank(article_id, text=None):
   global reference_summary_list, system_summary_list
 
-  text = "Thomas A. Anderson is a man living two lives. By day he is an " + \
-  "average computer programmer and by night a hacker known as " + \
-  "Neo. Neo has always questioned his reality, but the truth is " + \
-  "far beyond his imagination. Neo finds himself targeted by the " + \
-  "police when he is contacted by Morpheus, a legendary computer " + \
-  "hacker branded a terrorist by the government. Morpheus awakens " + \
-  "Neo to the real world, a ravaged wasteland where most of " + \
-  "humanity have been captured by a race of machines that live " + \
-  "off of the humans' body heat and electrochemical energy and " + \
-  "who imprison their minds within an artificial reality known as " + \
-  "the Matrix. As a rebel against the machines, Neo must return to " + \
-  "the Matrix and confront the agents: super-powerful computer " + \
-  "programs devoted to snuffing out Neo and the entire human " + \
-  "rebellion. "
+  if(text == None):
+    text = "Thomas A. Anderson is a man living two lives. By day he is an " + \
+    "average computer programmer and by night a hacker known as " + \
+    "Neo. Neo has always questioned his reality, but the truth is " + \
+    "far beyond his imagination. Neo finds himself targeted by the " + \
+    "police when he is contacted by Morpheus, a legendary computer " + \
+    "hacker branded a terrorist by the government. Morpheus awakens " + \
+    "Neo to the real world, a ravaged wasteland where most of " + \
+    "humanity have been captured by a race of machines that live " + \
+    "off of the humans' body heat and electrochemical energy and " + \
+    "who imprison their minds within an artificial reality known as " + \
+    "the Matrix. As a rebel against the machines, Neo must return to " + \
+    "the Matrix and confront the agents: super-powerful computer " + \
+    "programs devoted to snuffing out Neo and the entire human " + \
+    "rebellion. "
 
-  [dictionary, proc_text] = save_word_dict(text)
-
+  [dictionary, proc_text, sentences] = save_word_dict(text)
   raw_corpus = [dictionary.doc2bow(t) for t in proc_text]
 
-  print raw_corpus
-
-  tfidf = models.TfidfModel(raw_corpus)
-  
-  # print tfidf
+  tfidf        = models.TfidfModel(raw_corpus)
   corpus_tfidf = tfidf[raw_corpus]
-  print corpus_tfidf
+  simMat       = similarities.MatrixSimilarity(tfidf[raw_corpus])
+  similarityMatrix = simMat[corpus_tfidf]
+  
+  converged = False
+  
+  W = {}
+  for i, s1 in enumerate(simMat):
+    for j, s2 in enumerate(s1):
+      W[(i, j)] = s2
 
-  print corpus_tfidf[1]
-  # print dictionary
-  # load from   
+  ranks = [1.0]*len(proc_text)
+  temp_ranks = [1.0]*len(proc_text)
+  damping_factor = 0.5
+  num_sent = len(proc_text)
 
-  print 'Input text:'
-  print text
-  print
+  while(not converged):
+    for i in range(0, num_sent):
+      total_sum  = 0.0
+      for j in range(0, num_sent):
+        if(j == i):
+          continue
+        # print "[" + str(i) + ", " + str(j) +"] : ", s2, " ",
+        if(j < i):
+          W[(j, i)] = similarityMatrix[j][i]
+        else:
+          W[(j, i)] = similarityMatrix[i][j]
+        
+        den_sum = 0.0
+        for k in range(0, num_sent):
+          if(k == j):
+            continue
+          if(j < k):
+            W[(j, k)] = similarityMatrix[j][k]
+          else:
+            W[(j, k)] = similarityMatrix[k][j]
+          den_sum = den_sum + W[(j, k)]
+        # end k loop
+        
+        if(den_sum != 0.0):
+          total_sum = total_sum + (W[(j, i)] * ranks[j] / den_sum)
+      # end j loop
+      rank = (1 - damping_factor) + (damping_factor * total_sum)
+      temp_ranks[i] = rank
+    # end i loop
 
+    if(temp_ranks == ranks):
+      converged = True  
+    else:
+      ranks = temp_ranks
+    
+  # end while loop
+
+  ranked_sentences = sorted(range(len(ranks)),key=lambda x:ranks[x], reverse=True)
+  limit = 1 # to limit number of sentences
+  
+  result_summary = ''
+  for i in range(0, limit):
+    result_summary = result_summary + ' ' + sentences[ranked_sentences[i]]
+
+  # print result_summary
   reference_summary = summarize(text)
-  system_summary = "Morpheus wakes Neo into the real world that has been captured by machines."
-  article_id = "test" # this should be filename id
+
+  # system_summary = "Morpheus wakes Neo into the real world that has been captured by machines."
+  system_summary = result_summary
+  # article_id = "test" # this should be filename id
   
   # write reference summary to file
   ref_dir = os.pardir + "/test-summarization/reference/" + article_id + "_" + "reference.txt"
@@ -88,7 +134,6 @@ def start_page_rank():
   # write system summary to file
   write_to_file(sys_dir, system_summary)
   system_summary_list.append(sys_dir)
-
   test_print(reference_summary, system_summary)
 
 def test_print(reference_summary, system_summary):
@@ -102,12 +147,39 @@ def write_to_file(filename, summary):
   with codecs.open(filename, 'w', encoding='utf8') as f:
     f.write(str(summary.encode('ascii', errors='ignore')))
 
+def generate_score():
+  global reference_summary_list, system_summary_list
+  training = ET.parse('../data/training.xml')
+
+  files_read = 0
+  # reference_summary_list = []
+  # system_summary_list = []
+
+  for i, t in enumerate(training.findall("article")):
+    article_id = t.get('id')
+    reference_summary = t.find("summary").text
+    text = t.find("text").text
+    try:
+      system_summary = summarize(text, word_count = 20)
+    except (ValueError, ZeroDivisionError):
+      continue 
+
+    if(system_summary == None or len(system_summary) > 140 or len(system_summary) == 0 ):
+      continue
+    # call page_rank
+    start_page_rank(article_id, text)
+
+    if (files_read > 10):
+      break
+
+    files_read = files_read + 1
+
 def main():
   global system_summary_list, reference_summary_list
   system_summary_list = []
   reference_summary_list = []
 
-  start_page_rank()
+  generate_score()
   recall_list,precision_list,F_measure_list = PythonROUGE(parent_dir, system_summary_list,reference_summary_list, 1)
   print ('recall = ' + str(recall_list))
   print ('precision = ' + str(precision_list))
